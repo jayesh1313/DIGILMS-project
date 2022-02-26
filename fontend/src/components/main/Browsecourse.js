349e1970ca8896ea333a7a82ee94b6eebaaed572
PramitSrivastava <pramitsrivastava476@gmail.com>
2022-02-26 23:55:53 +0530

    .

--- fontend/src/components/main/Browsecourse.js
+++ fontend/src/components/main/Browsecourse.js
@@ -17,16 +17,69 @@ export default function BrowseCourse() {
           <CardMedia
             component="img"
             height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
+            image="https://www.onlinecoursereport.com/wp-content/uploads/2020/07/shutterstock_394793860-1024x784.jpg"
             alt="green iguana"
           />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
-              Lizard
+             Web Development
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Lizards are a widespread group of squamate reptiles, with over 6,000
-              species, ranging across all continents except Antarctica
+             Full mern stack developer
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+      </Grid>
+    )
+  };
+  const displayCourses1 = () => {
+    return (
+      <Grid item>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://www.dataquest.io/wp-content/uploads/2019/05/what-is-data-science-1.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+             Data Science
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+             Data Science
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+      </Grid>
+    )
+  };
+   const displayCourses2 = () => {
+    return (
+      <Grid item>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://www.softwaretestinghelp.com/wp-content/qa/uploads/2020/06/Ethical-Hacking.png"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+            Ethical Hacking
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+             Become a ethical hacker
             </Typography>
           </CardContent>
           <CardActions>
@@ -38,6 +91,145 @@ export default function BrowseCourse() {
       </Grid>
     )
   }
+  const displayCourses3 = () => {
+    return (
+      <Grid item>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://www.appschopper.com/blog/wp-content/uploads/2021/01/Android-App-Development-Frameworks-AppsChopper.png"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Android develeopment
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+             Become a android developer
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+      </Grid>
+    )
+  }
+  const displayCourses4 = () => {
+    return (
+      <Grid item>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            
+            image="https://stackify.com/wp-content/uploads/2018/08/Feature-Image-Python-Frameworks-1.png"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Python
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+             Learn python
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+      </Grid>
+    )
+  };
+  const displayCourses5 = () => {
+    return (
+      <Grid item>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://imageio.forbes.com/specials-images/imageserve/60269ef7ada7085ce688069b/Cyber-Security-and-Digital-Data-Protection-Concept/960x0.jpg?fit=bounds&format=jpg&width=960"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Cyber security
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+             Become a android developer
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+      </Grid>
+    )
+  }
+
+  const displayCourses6 = () => {
+    return (
+      <Grid item>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwamMtJ87fKPMFsN7wkFSRBqN-SzN6fNaIKWLmnCBF-laeShN_oN4mqV68QI-CHU6d5LU&usqp=CAU"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Java
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+             Become a java developer
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+      </Grid>
+    )
+  }
+  const displayCourses7 = () => {
+    return (
+      <Grid item>
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image="https://newsignature.com/wp-content/uploads/2016/05/dotnet.jpg"
+            alt="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Java
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+             Become a java developer
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Share</Button>
+            <Button size="small">Learn More</Button>
+          </CardActions>
+        </Card>
+
+      </Grid>
+    )
+  }
+  
+
 
   return (
     <Paper>
@@ -55,8 +247,14 @@ export default function BrowseCourse() {
         <Container>
           <Grid container spacing={5}>
             {displayCourses()}
-            {displayCourses()}
-            {displayCourses()}
+            {displayCourses1()}
+            {displayCourses2()}
+            {displayCourses3()}
+            {displayCourses5()}
+            {displayCourses4()}
+            {displayCourses6()}
+            {displayCourses7()}
+           
           </Grid>
         </Container>
 
