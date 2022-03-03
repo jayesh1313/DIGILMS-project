43843f3042b2aa2f1c79bdade306411f30b1130d
jayesh1313 <jayeshgg2@gmail.com>
2022-03-03 18:01:45 +0530

    ....

---
+++ fontend/src/components/trainer/BrowseTrainer.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import '../../stylesheet/BrowseTrainer.css'
+export default function BrowseTrainer() {
+  return (
+    <div>
+        
+<main class="page-content">
+  <div class="card">
+    <div class="content">
+      <h2 class="title">Web developer</h2>
+      <p class="copy">Dr.Rahul</p>
+      <button class="btn">View Course</button>
+    </div>
+  </div>
+  <div class="card">
+    <div class="content">
+      <h2 class="title">Machine Learning</h2>
+      <p class="copy">Proff. Yash</p>
+      <button class="btn">View Course</button>
+    </div>
+  </div>
+  <div class="card">
+    <div class="content">
+      <h2 class="title">Ethical HAcking</h2>
+      <p class="copy">Dr. Ritik</p>
+      <button class="btn">View Course</button>
+    </div>
+  </div>
+  <div class="card">
+    <div class="content">
+      <h2 class="title">Android developer</h2>
+      <p class="copy">Dr. P.k</p>
+      <button class="btn">View Course</button>
+    </div>
+  </div>
+</main>
+    </div>
+  )
+}
