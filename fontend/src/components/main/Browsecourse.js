98b87c5ada114562da2c5712f90dee353aa284f0
m-triple-m <triplem656@gmail.com>
2022-03-06 19:24:11 +0530

    ..

--- fontend/src/components/main/Browsecourse.js
+++ fontend/src/components/main/Browsecourse.js
@@ -1,82 +1,41 @@
-import React, { useState } from 'react'
-import app_config from '../../config';
+import React, { useEffect, useState } from "react";
+import app_config from "../../config";
 
-import'../../stylesheet/BrowseTrainer.css';
+import "../../stylesheet/BrowseTrainer.css";
 
 export default function BrowseCourse() {
   const url = app_config.api_url;
 
   const [courseList, setCourseList] = useState([]);
-  const [loading, setLoading] = useState(true)
-
-
+  const [loading, setLoading] = useState(true);
 
   const fetchCourses = () => {
-      // fetch(ur)
-  }
+    fetch(url + "/courses/getall")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+        setCourseList(data);
+      });
+  };
 
- 
-    return (
-      <div>
-      <div class="card">
-    <div class="content">
-      <h2 class="title">Web developer</h2>
-      <p class="copy">Dr.Rahul</p>
-      <button class="btn">View Course</button>
-    </div>
-  </div>
-  <div class="card">
-    <div class="content">
-      <h2 class="title">Machine Learning</h2>
-      <p class="copy">Proff. Yash</p>
-      <button class="btn">View Course</button>
-    </div>
-  </div>
-  <div class="card">
-    <div class="content">
-      <h2 class="title">Ethical HAcking</h2>
-      <p class="copy">Dr. Ritik</p>
-      <button class="btn">View Course</button>
-    </div>
-  </div>
-  <div class="card">
-    <div class="content">
-      <h2 class="title">Android developer</h2>
-      <p class="copy">Dr. P.k</p>
-      <button class="btn">View Course</button>
-    </div>
-  </div>
-  <div class="card">
-    <div class="content">
-      <h2 class="title">Web developer</h2>
-      <p class="copy">Dr.Gaurav Taneja </p>
-      <button class="btn">View Course</button>
-    </div>
-  </div>
-  <div class="card">
-    <div class="content">
-      <h2 class="title">Machine Learning</h2>
-      <p class="copy">Proff. Yash</p>
-      <button class="btn">View Course</button>
-    </div>
-  </div>
-  <div class="card">
-    <div class="content">
-      <h2 class="title">Ethical HAcking</h2>
-      <p class="copy">Dr. Ritik</p>
-      <button class="btn">View Course</button>
-    </div>
-  </div>
-  <div class="card">
-    <div class="content">
-      <h2 class="title">Android developer</h2>
-      <p class="copy">Dr. P.k</p>
-      <button class="btn">View Course</button>
-    </div>
-  </div>
-  </div>
+  useEffect(() => {
+    fetchCourses();
+  }, []);
 
-      
-  )
+  const showCourses = () => {
+    if (!loading) {
+      return courseList.map((course) => (
+        <div class="card">
+          <div class="content">
+            <h2 class="title">Web developer</h2>
+            <p class="copy">Dr.Rahul</p>
+            <button class="btn">View Course</button>
+          </div>
+        </div>
+      ));
+    }
+  };
 
-}}
+  return <div>{showCourses()}</div>;
+}
