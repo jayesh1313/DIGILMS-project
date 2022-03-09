f825a1ad41a778a86a79df84de62b3c0257bfc2e
PramitSrivastava <pramitsrivastava476@gmail.com>
2022-03-09 07:33:29 +0530

    .

--- fontend/src/components/main/Home.js
+++ fontend/src/components/main/Home.js
@@ -1,46 +1,15 @@
 import React from 'react'
-
+import '../../stylesheet/Home.css'
 export default function Home() {
   return (
     <div>
-      <!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.88.1">
-    <title>Carousel Template · Bootstrap v5.1</title>
+     
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
 
     
 
-    <!-- Bootstrap core CSS -->
-<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="carousel.css" rel="stylesheet">
-  </head>
-  <body>
     
 <header>
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
@@ -62,7 +31,7 @@ export default function Home() {
           </li>
         </ul>
         <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
           <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
       </div>
@@ -70,7 +39,7 @@ export default function Home() {
   </nav>
 </header>
 
-<main>
+
 
   <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
@@ -124,13 +93,11 @@ export default function Home() {
   </div>
 
 
-  <!-- Marketing messaging and featurettes
-  ================================================== -->
-  <!-- Wrap the rest of the page in another container to center all the content. -->
+ 
 
   <div class="container marketing">
 
-    <!-- Three columns of text below the carousel -->
+    
     <div class="row">
       <div class="col-lg-4">
         <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
@@ -138,27 +105,27 @@ export default function Home() {
         <h2>Heading</h2>
         <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
         <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
-      </div><!-- /.col-lg-4 -->
+      </div>
       <div class="col-lg-4">
         <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
 
         <h2>Heading</h2>
         <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
         <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
-      </div><!-- /.col-lg-4 -->
+      </div>
       <div class="col-lg-4">
         <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#777"/><text x="50%" y="50%" fill="#777" dy=".3em">140x140</text></svg>
 
         <h2>Heading</h2>
         <p>And lastly this, the third column of representative placeholder content.</p>
         <p><a class="btn btn-secondary" href="#">View details &raquo;</a></p>
-      </div><!-- /.col-lg-4 -->
-    </div><!-- /.row -->
+      </div>
+    </div>
 
 
-    <!-- START THE FEATURETTES -->
+   
 
-    <hr class="featurette-divider">
+    <hr class="featurette-divider"/>
 
     <div class="row featurette">
       <div class="col-md-7">
@@ -171,7 +138,7 @@ export default function Home() {
       </div>
     </div>
 
-    <hr class="featurette-divider">
+    <hr class="featurette-divider"/>
 
     <div class="row featurette">
       <div class="col-md-7 order-md-2">
@@ -184,7 +151,7 @@ export default function Home() {
       </div>
     </div>
 
-    <hr class="featurette-divider">
+    <hr class="featurette-divider"/>
 
     <div class="row featurette">
       <div class="col-md-7">
@@ -197,27 +164,28 @@ export default function Home() {
       </div>
     </div>
 
-    <hr class="featurette-divider">
-
-    <!-- /END THE FEATURETTES -->
-
-  </div><!-- /.container -->
+    <hr class="featurette-divider"/>
 
 
-  <!-- FOOTER -->
+  
+
+
+ 
   <footer class="container">
     <p class="float-end"><a href="#">Back to top</a></p>
     <p>&copy; 2017–2021 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
   </footer>
-</main>
+
 
 
     <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
 
       
-  </body>
-</html>
+
 
     </div>
+    </div>
+    
+    
   )
-}
+  }
