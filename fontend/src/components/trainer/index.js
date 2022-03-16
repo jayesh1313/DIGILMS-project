1558ca32e20c5ec45a239eefcafd93b8dc65a679
jayesh1313 <jayeshgg2@gmail.com>
2022-03-16 09:30:28 +0530

    ...

--- fontend/src/components/trainer/index.js
+++ fontend/src/components/trainer/index.js
@@ -1,12 +1,12 @@
 import { Outlet } from "react-router-dom";
-// import Footer from "./footer";
+import Footer from "./Footer";
 import Header from "./Header";
 const Trainer = () => {
   return (
     <>
     <Header></Header>
       <Outlet />
-      {/* <footer></footer> */}
+     <Footer></Footer>
     </>
   );
 };
--- fontend/src/components/user/index.js
+++ fontend/src/components/user/index.js
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom";
-
+import Footer from "./Footer";
 const User = () => {
   return (
     <>
       <Outlet />
+      <Footer></Footer>
     </>
   );
 };
