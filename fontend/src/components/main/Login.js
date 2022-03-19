c6b5fa64c70b52e3a75f608db549102aab9f215a
jayesh1313 <jayeshgg2@gmail.com>
2022-03-19 16:15:32 +0530

    ...

--- fontend/src/components/main/Login.js
+++ fontend/src/components/main/Login.js
@@ -5,7 +5,9 @@ import app_config from "../../config";
 import Swal from "sweetalert2";
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
-
+import FacebookLogin from 'react-facebook-login';
+import ReactDOM from 'react-dom';
+import GoogleLogin from 'react-google-login';
 export default function Login() {
   const url = app_config.api_url;
 
@@ -13,6 +15,13 @@ export default function Login() {
     email: "",
     password: "",
   };
+  const responseGoogle = (response) => {
+    console.log(response);
+  }
+  const responseFacebook = (response) => {
+    console.log(response);
+  }
+  
 
   const submituser = (formdata) => {
     console.log(formdata);
@@ -43,8 +52,25 @@ export default function Login() {
         <Formik initialValues={userForm} onSubmit={submituser}>
           {({ values, handleChange, handleSubmit }) => (
             <form onSubmit={handleSubmit}>
-              
-
+              ReactDOM.render(
+  <GoogleLogin
+    clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
+    buttonText="Login"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />,
+  document.getElementById('googleButton')
+);
+ReactDOM.render(
+  <FacebookLogin
+    appId="1088597931155576"
+    autoLoad={true}
+    fields="name,email,picture"
+    // onClick={componentClicked}
+    callback={responseFacebook} />,
+  document.getElementById('demo')
+);
               <div class="user-box">
                 <input
                   type="email"
