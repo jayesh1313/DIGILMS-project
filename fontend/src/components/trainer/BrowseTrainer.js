beb69a632fac10ac927bce5d07e6e7f9fdad4751
m-triple-m <triplem656@gmail.com>
2022-04-09 19:01:18 +0530

    ..

--- fontend/src/components/trainer/BrowseTrainer.js
+++ fontend/src/components/trainer/BrowseTrainer.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import app_config from '../../config';
-import '../../stylesheet/BrowseTrainer.css'
+import React, { useEffect, useState } from "react";
+import app_config from "../../config";
+import "../../stylesheet/BrowseTrainer.css";
 export default function BrowseTrainer() {
-
   const url = app_config.api_url;
 
   const [TrainerList, setTrainerList] = useState([]);
@@ -25,26 +24,20 @@ export default function BrowseTrainer() {
   const showTrainers = () => {
     if (!loading) {
       return TrainerList.map((Trainer) => (
-        
         <div class="card">
-    <div class="content">
-      <h2 class="title">{Trainer.fullname}</h2>
-      <p class="copy">Dr.Rahul</p>
-      <button class="btn">View Trainer</button>
-    </div>
-    </div>
+          <div class="content">
+            <h2 class="title">{Trainer.fullname}</h2>
+            <p class="copy">Dr.Rahul</p>
+            <button class="btn">View Trainer</button>
+          </div>
+        </div>
       ));
     }
   };
 
   return (
     <div>
-        
-<main class="page-content">
-  
-  {showTrainers()}
-  
-</main>
+      <main class="page-content">{showTrainers()}</main>
     </div>
-  )
+  );
 }
