237df493b86c1ee7e391bfa8f6713195fcca387f
jayesh1313 <jayeshgg2@gmail.com>
2022-04-09 09:24:31 +0530

    ..

--- fontend/src/components/main/Login.js
+++ fontend/src/components/main/Login.js
@@ -39,7 +39,7 @@ export default function Login() {
         Swal.fire({
           icon: "success",
           title: "success",
-          text: "user Added Successfully",
+          text: "user Login Successfully",
         });
       });
   };
