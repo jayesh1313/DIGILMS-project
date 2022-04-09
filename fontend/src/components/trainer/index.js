9c2ecf87863b3ccbcef17c4afeac3dd5818ac90b
jayesh1313 <jayeshgg2@gmail.com>
2022-04-09 18:53:17 +0530

    ....

--- fontend/src/components/trainer/index.js
+++ fontend/src/components/trainer/index.js
@@ -1,10 +1,30 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../SideBar";
 
+import { AccountCircle, Book, Dashboard, HowToReg } from "@mui/icons-material";
+
+
 import Footer from "./Footer";
 import Header from "./Header";
 import { Slider } from "@mui/material";
 const Trainer = () => {
+  const sideOptions = [
+    {
+      name: "Dashboard",
+      icon: <Dashboard />,
+      link: "/admin/dashboard",
+    },
+    {
+      name: "Trainer Manager",
+      icon: <HowToReg />,
+      link: "/trainer/BrowseTrainer",
+    },
+    {
+      name: "Add Course",
+      icon: <Book />,
+      link: "/trainer/Addcourse",
+    },
+  ];
   return (
     <>
       <Sidebar>
