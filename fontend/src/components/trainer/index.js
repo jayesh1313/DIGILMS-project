e8e8a1eaec8604243b01c365ad88c1d760bc6748
m-triple-m <triplem656@gmail.com>
2022-04-09 18:14:13 +0530

    ..

--- fontend/src/components/trainer/index.js
+++ fontend/src/components/trainer/index.js
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
+import Sidebar from "../SideBar";
 
 import Footer from "./Footer";
 import Header from "./Header";
 const Trainer = () => {
   return (
     <>
-      <Header></Header>
-
-      <Outlet />
-      <Footer></Footer>
+      <Sidebar>
+        <Outlet />
+      </Sidebar>
     </>
   );
 };
